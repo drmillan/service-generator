59997542a107e83ea827f8c3e1346384c4be1d3a
awayo <fsotop@aunder.org>
2014-01-10 14:21:08 +0100

    Move to template 5.0

---
+++ templates/ios/5.0/static/Classes/gen/Base/LogicFilter.h
@@ -0,0 +1,19 @@
+//
+//  LogicFilter.h
+//  PeopleSports
+//
+//  Created by DRM on 22/10/13.
+//  Copyright (c) 2013 Mobivery. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LogicFilter : NSObject
+- (NSString *) preInjectURLParameters:(NSString *)url withObject:(NSObject *)obj onService:(NSString *)serviceName onMethod:(NSString *)methodName;
+- (NSString *) postInjectURLParameters:(NSString *)url withObject:(NSObject *)obj onService:(NSString *)serviceName onMethod:(NSString *)methodName;
+- (NSString *) escapeUrl:(NSString *)url onService:(NSString *)serviceName onMethod:(NSString *)methodName;
+- (NSMutableURLRequest *)manageRequest:(NSMutableURLRequest *)request onService:(NSString *)serviceName onMethod:(NSString *)methodName;
+- (NSString *) preprocessResponse:(NSString *)responseString onService:(NSString *)serviceName onMethod:(NSString *)methodName;
+- (NSDictionary *) preprocessResponseAsDictionary:(NSDictionary *)responseDict onService:(NSString *)serviceName onMethod:(NSString *)methodName;
+- (UIView *) loadingViewForTasks;
+@end
