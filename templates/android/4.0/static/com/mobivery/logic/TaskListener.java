8e9914c4956722b127922b64a9ad717a8bbbde15
Nacho L <mrm@aunder.es>
2014-02-04 20:29:20 +0100

    Created a new template for Android
    Template 4.0 features more parameters in the hooks. All of them have now logic/method attributes, so we can create more advanced filters like a filter for swapping at runtime the entrypoints with mock ones.

---
+++ templates/android/4.0/static/com/mobivery/logic/TaskListener.java
@@ -0,0 +1,23 @@
+package com.mobivery.logic;
+
+import com.mobivery.utils.ResponseInfo;
+
+/**
+ * Generated class, do not touch
+ * Created by Mobivery
+ */
+public interface TaskListener<T,Q> {
+    /**
+	 * Task completed callback (with ResponseInfo)
+	 * @param request RequestDTO
+	 * @param response ResponseDTO
+     * @param responseInfo Response metadata (to get http header errors)
+     */
+    public void onServiceResponse(T request,Q response,ResponseInfo responseInfo);
+    /**
+     * Task error callback
+     * @param request RequestDTO
+     * @param ex ResponseDTO
+     */
+    public void onServiceError(T request,Exception ex);
+}
